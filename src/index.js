3c2938926f1e6364ddf253144439d6207bffa81e
Clement <ziwei.liu@cca.edu>
2020-07-02 17:50:44 -0700

    test

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const simpleGit = require("simple-git");
-const git = simpleGit();
-
-git
-  .init()
-  .add("./dist/*")
-  .commit("Test commit!")
-  .addRemote("origin", "https://github.com/ClementLiu/test.git")
-  .push("origin", "master");
