6769fa3e765bae592b0abeba3d1cc34a29896a73
Clement <ziwei.liu@cca.edu>
2020-07-02 17:18:03 -0700

    test master

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const simpleGit = require("simple-git");
+const git = simpleGit(__dirname);
+
+git
+  .checkIsRepo()
+  .then((isRepo) => !isRepo && initialiseRepo(git))
+  .then(() => git.fetch());
+
+function initialiseRepo(git) {
+  return git
+    .init()
+    .then(() => git.addRemote("master", "https://github.com/user/repo.git"));
+}
+console.log("wokring");
+/* 
 require("simple-git")()
   .init()
   .add("./*")
@@ -6,3 +21,4 @@ require("simple-git")()
   .push("origin", "master");
 
 console.log("wokring");
+ */
